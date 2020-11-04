04a3176e0b7ab48566c093d7acac17aa948848de
june <junearagoncillo@gmail.com>
2020-11-04 17:17:46 +0800

    basic crud

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
+import RadListView from 'nativescript-ui-listview/vue';
 
+Vue.use(RadListView);
 new Vue({
     render: h => h('frame', [h(Home)]),
 }).$start();
